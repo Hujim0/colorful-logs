53c3f843dce80c8598c2f11301dd7b271192108a
hujim0 <david.grigorev.2015@mail.ru>
2025-03-29 00:16:15 +0300

    refactor!: separate lib and console builds

--- colorfulLogs.Tests/PatterMatcherTests.cs
+++ colorfulLogs.Tests/PatterMatcherTests.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
-using colorfulLogs.database;
 using colorfulLogs.patterns;
 using colorfulLogs.structs;
 
